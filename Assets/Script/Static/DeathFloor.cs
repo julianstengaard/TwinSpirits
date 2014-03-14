fa16f20c592c714516c9e12578fab3600374d113
Mathias Vielwerth <onimishra@gmail.com>
2014-03-14 15:09:02 +0100

    - Add: Death floor of death. Removes all GameObjects that'll fall over the side.

---
+++ Assets/Script/Static/DeathFloor.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+public class DeathFloor : MonoBehaviour {
+	void OnTriggerEnter(Collider other) {
+		other.gameObject.SendMessage("TakeDamage", 10000f);
+		GameObject.Destroy(other.gameObject);
+
+		if(other.GetComponent<Hero>() != null)
+			Application.LoadLevel(0);
+	}
+}
