755437875d6f91f6e0e7758105e0a92206eda646
Mathias Vielwerth <onimishra@gmail.com>
2014-03-25 14:43:30 +0100

    - Add: New items - Add: New powers - Add: AI behaviour that checks if the AI unit is alive - Add: More smoke! - Add: New sound system for random playing of sounds

---
+++ Assets/Script/Collectables/Items/CollectableUltimateSacrifice.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+// This will transfor all Damage effects too!	
+public class CollectableUltimateSacrifice : Collectable {
+	#region implemented abstract members of Collectable
+	public override void Collected (Hero collector)	{
+		var list = collector.GetEffectsFromWeapons().ToArray();
+		foreach(var effect in list) {
+			collector.otherPlayer.AddEffectToWeapons(effect);
+			collector.RemoveEffectFromWeapon(effect);
+		}
+		collector.AddEffectToWeapons(new Damage(15));
+	}
+	#endregion
+}
---
+++ Assets/Script/Effects/ShieldChance.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShieldChance : Effect 
+{
+	private float _chance;
+	private Hero hero;
+	private bool isShieldUp = false;
+	private GameObject shield;
+
+	public ShieldChance(float chance) {
+		_chance = chance;
+	}
+
+	public override void DoEffect (BaseUnit target, GameObject source, Vector3 attackPosition, ref float damage) {
+		if(isShieldUp || Random.Range (0f, 1f) >= _chance)
+			return;
+
+		hero = source.GetComponent<Hero>();
+
+		if(hero == null) {
+			Debug.Log ("Unable to find hero");
+			return;
+		}
+		
+		isShieldUp = true;
+		hero.immortal = true;
+		shield = SpiritImmortal.CreateShieldMesh(new Color(0f, 0.6f, 0f, 0.5f), hero.transform);
+		shield.transform.parent = hero.transform;
+		var timer = shield.AddComponent<SimpleSelfTimer>();
+		timer.trigger += RemoveEffect;
+		timer.timeout = 5f;
+	}	
+	
+	public override IEnumerator DoEffectCoroutine (BaseUnit target, GameObject source, Vector3 attackPosition) {
+//		yield return new WaitForSeconds(5);
+//		RemoveEffect();
+		yield break;
+	}
+
+	private void RemoveEffect() {
+		GameObject.Destroy(shield);
+		hero.immortal = false;
+		isShieldUp = false;
+	}
+
+	public override int GetPriority() {
+		return 1;
+	}
+
+	public override bool IsSplashable () {
+		return false;
+	}
+}
