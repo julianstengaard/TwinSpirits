16dfa46ad1302937fcdb1d2dd66cd21323a790ed
julianstengaard <julianstengaard@gmail.com>
2014-04-03 13:55:42 +0200

    - Change: Level creation now uses bridge connection points on islands (= much easier for level designers to add new islands). These connection points can be "forced", meaning the island will ONLY be used, if there are bridges connecting to them.

---
+++ Assets/Script/MapGeneration/BridgeOpening.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum Opening {
+	LEFT,
+	TOP,
+	RIGHT, 
+	BOT
+}
+
+public class BridgeOpening : MonoBehaviour {
+	public Opening opening;
+	public bool forced = false;
+}
--- Assets/Script/MapGeneration/CreateLevel.cs
+++ Assets/Script/MapGeneration/CreateLevel.cs
@@ -16,25 +16,27 @@ public class CreateLevel : MonoBehaviour {
 	public GameObject BossCell;
 	private bool BossCellInstansiated;
 
-	public GameObject[] mazeCellPrefabs0000;
-	public GameObject[] mazeCellPrefabs0001;
-	public GameObject[] mazeCellPrefabs0010;
-	public GameObject[] mazeCellPrefabs0011;
+	public GameObject[] IslandsWithBridgeOpenings;
 
-	public GameObject[] mazeCellPrefabs0100;
-	public GameObject[] mazeCellPrefabs0101;
-	public GameObject[] mazeCellPrefabs0110;
-	public GameObject[] mazeCellPrefabs0111;
+	private GameObject[] mazeCellPrefabs0000;
+	private GameObject[] mazeCellPrefabs0001;
+	private GameObject[] mazeCellPrefabs0010;
+	private GameObject[] mazeCellPrefabs0011;
 
-	public GameObject[] mazeCellPrefabs1000;
-	public GameObject[] mazeCellPrefabs1001;
-	public GameObject[] mazeCellPrefabs1010;
-	public GameObject[] mazeCellPrefabs1011;
+	private GameObject[] mazeCellPrefabs0100;
+	private GameObject[] mazeCellPrefabs0101;
+	private GameObject[] mazeCellPrefabs0110;
+	private GameObject[] mazeCellPrefabs0111;
 
-	public GameObject[] mazeCellPrefabs1100;
-	public GameObject[] mazeCellPrefabs1101;
-	public GameObject[] mazeCellPrefabs1110;
-	public GameObject[] mazeCellPrefabs1111;
+	private GameObject[] mazeCellPrefabs1000;
+	private GameObject[] mazeCellPrefabs1001;
+	private GameObject[] mazeCellPrefabs1010;
+	private GameObject[] mazeCellPrefabs1011;
+
+	private GameObject[] mazeCellPrefabs1100;
+	private GameObject[] mazeCellPrefabs1101;
+	private GameObject[] mazeCellPrefabs1110;
+	private GameObject[] mazeCellPrefabs1111;
 
 	public GameObject bridgeSpanPrefab;
 	public GameObject bridgeEndPrefab;
@@ -58,7 +60,7 @@ public class CreateLevel : MonoBehaviour {
 			mazeLength = levelInfo.GetComponent<LevelCreationInfo>().levelLength;
 		}
 
-		ImportCellPrefabs();
+
 		mazeFromX = Mathf.FloorToInt(mazeWidth/2);
 		mazeFromY = mazeHeight-1;
 
@@ -78,6 +80,9 @@ public class CreateLevel : MonoBehaviour {
 	private void CreateMaze()
 	{
 		level = new Maze(mazeWidth, mazeHeight);
+		PopulateIslandArrays();
+		ImportCellPrefabs();
+
 		level.GenerateMazeFromCell(mazeFromX, mazeFromY, mazeLength, maxPathIterationLenght);
 		
 		InstantiateMaze(level);
@@ -111,6 +116,7 @@ public class CreateLevel : MonoBehaviour {
 			for (int x = 0; x < maze.width; x++)
 			{
 				int dictionaryKey = ConvertDoorsToDictionaryKey(maze.GetCell(x, y).doors);
+				//If start cell
 				if(x == mazeFromX && y == mazeFromY) {
 					Vector3 randomizedCellPosition = GetRandomCellDisplacement(x * cellSpacing, -y * cellSpacing, cellSpacing);
 					mazeCellInstances[x,y] = (GameObject)GameObject.Instantiate(StartCell, randomizedCellPosition, Quaternion.identity);
@@ -118,11 +124,85 @@ public class CreateLevel : MonoBehaviour {
 				else if ( !maze.GetCell(x, y).HasNoDoors() ) 
 				{
 					Vector3 randomizedCellPosition = GetRandomCellDisplacement(x * cellSpacing, -y * cellSpacing, cellSpacing);
-					var cells = (GameObject[]) mazeCellPrefabs[dictionaryKey];
-					mazeCellInstances[x,y] = InstantiateCell(cells , Random.Range (0, cells.Length), randomizedCellPosition, maze, x, y);
+					//print ("ins: " + dictionaryKey);
+					//var cells = (GameObject[]) mazeCellPrefabs[dictionaryKey];
+					var validIslands = (GameObject[]) mazeCellPrefabs[dictionaryKey];
+					mazeCellInstances[x,y] = InstantiateCell(validIslands , Random.Range (0, validIslands.Length), randomizedCellPosition, maze, x, y);
 				}
 			}
 		}
+	}
+
+	private void PopulateIslandArrays() {
+		mazeCellPrefabs0000 = FindIslandsWithDoors(false, false, false, false);
+		mazeCellPrefabs0001 = FindIslandsWithDoors(false, false, false, true);
+		mazeCellPrefabs0010 = FindIslandsWithDoors(false, false, true, false);
+		mazeCellPrefabs0011 = FindIslandsWithDoors(false, false, true, true);
+		
+		mazeCellPrefabs0100 = FindIslandsWithDoors(false, true, false, false);
+		mazeCellPrefabs0101 = FindIslandsWithDoors(false, true, false, true);
+		mazeCellPrefabs0110 = FindIslandsWithDoors(false, true, true, false);
+		mazeCellPrefabs0111 = FindIslandsWithDoors(false, true, true, true);
+		
+		mazeCellPrefabs1000 = FindIslandsWithDoors(true, false, false, false);
+		mazeCellPrefabs1001 = FindIslandsWithDoors(true, false, false, true);
+		mazeCellPrefabs1010 = FindIslandsWithDoors(true, false, true, false);
+		mazeCellPrefabs1011 = FindIslandsWithDoors(true, false, true, true);
+		
+		mazeCellPrefabs1100 = FindIslandsWithDoors(true, true, false, false);
+		mazeCellPrefabs1101 = FindIslandsWithDoors(true, true, false, true);
+		mazeCellPrefabs1110 = FindIslandsWithDoors(true, true, true, false);
+		mazeCellPrefabs1111 = FindIslandsWithDoors(true, true, true, true);
+	}
+
+	private GameObject[] FindIslandsWithDoors(bool left, bool top, bool right, bool bot) {
+		ArrayList validIslands = new ArrayList();
+
+		foreach (GameObject island in IslandsWithBridgeOpenings) {
+			bool[] foundOpenings = {false, false, false, false};
+			bool[] forcedOpenings = {false, false, false, false};
+
+			BridgeOpening[] openings = island.transform.GetComponentsInChildren<BridgeOpening>(true);
+			//if (openings.Count() > 0)
+				//print(island.name);
+
+			//Are some openings forced?
+			foreach (BridgeOpening bridgeOpening in openings) {
+				if (bridgeOpening.opening == Opening.LEFT && bridgeOpening.forced)
+					forcedOpenings[0] = true;
+				if (bridgeOpening.opening == Opening.TOP && bridgeOpening.forced)
+					forcedOpenings[1] = true;
+				if (bridgeOpening.opening == Opening.RIGHT && bridgeOpening.forced)
+					forcedOpenings[2] = true;
+				if (bridgeOpening.opening == Opening.BOT && bridgeOpening.forced)
+					forcedOpenings[3] = true;
+			}
+
+			foreach (BridgeOpening bridgeOpening in openings) {
+				if ((!left && !forcedOpenings[0]) || (left && bridgeOpening.opening == Opening.LEFT))
+					foundOpenings[0] = true;
+				if ((!top && !forcedOpenings[1]) || (top && bridgeOpening.opening == Opening.TOP))
+					foundOpenings[1] = true;
+				if ((!right && !forcedOpenings[2]) || (right && bridgeOpening.opening == Opening.RIGHT))
+					foundOpenings[2] = true;
+				if ((!bot && !forcedOpenings[3]) || (bot && bridgeOpening.opening == Opening.BOT))
+					foundOpenings[3] = true;
+			}
+
+			if (foundOpenings[0] && foundOpenings[1] && foundOpenings[2] && foundOpenings[3])
+				validIslands.Add(island);
+		}
+
+		//Create the island array
+		GameObject[] returnIslands = new GameObject[validIslands.Count];
+
+		int i = 0;
+		foreach (GameObject validIsland in validIslands) {
+			returnIslands[i] = (GameObject) validIsland;
+			i++;
+		}
+		//print ("returning " + returnIslands.Length + " islands for " + left + "" + top + "" + right + "" + bot);
+		return returnIslands;
 	}
 
 	private void InstantiateMazeBridges(Maze maze)
@@ -133,26 +213,45 @@ public class CreateLevel : MonoBehaviour {
 			{
 				bool[] doors = maze.GetCell(x, y).doors;
 
+				GameObject island1Anchor;
+				GameObject island2Anchor;
+
 				//Only instantiate bridges right & down (to avoid double bridges)
-				if (doors[2])
-				{
-					mazeBridgeInstances.Add( InstantiateSpanningBridge(bridgeSpanPrefab, bridgeEndPrefab, 
-					    mazeCellInstances[x, y], mazeCellInstances[x+1, y]));
+				if (doors[2]) {
+					island1Anchor = FindBridgeAnchor(mazeCellInstances[x, y], Opening.RIGHT);
+					island2Anchor = FindBridgeAnchor(mazeCellInstances[x+1, y], Opening.LEFT);
+
+					mazeBridgeInstances.Add(InstantiateSpanningBridge(
+						bridgeSpanPrefab, bridgeEndPrefab, mazeCellInstances[x, y], mazeCellInstances[x+1, y], island1Anchor, island2Anchor));
 				}
-				if (doors[3])
-				{
-					mazeBridgeInstances.Add( InstantiateSpanningBridge(bridgeSpanPrefab, bridgeEndPrefab, 
-						mazeCellInstances[x, y], mazeCellInstances[x, y+1]));
+				if (doors[3]) {
+					island1Anchor = FindBridgeAnchor(mazeCellInstances[x, y], Opening.BOT);
+					island2Anchor = FindBridgeAnchor(mazeCellInstances[x, y+1], Opening.TOP);
+
+					mazeBridgeInstances.Add( InstantiateSpanningBridge(
+						bridgeSpanPrefab, bridgeEndPrefab, mazeCellInstances[x, y], mazeCellInstances[x, y+1], island1Anchor, island2Anchor));
 				}
 			}
 		}
 	}
 
+	private GameObject FindBridgeAnchor(GameObject island, Opening opening) {
+		BridgeOpening[] islandAnchors;
+		islandAnchors = island.GetComponentsInChildren<BridgeOpening>();
+
+		foreach (BridgeOpening anchor in islandAnchors) {
+			if (anchor.opening == opening)
+				return anchor.gameObject;
+		}
+		Debug.LogError("Couldn't find bridge anchor for island: " + island.name);
+		return null;
+	}
+
 	private Vector3 GetRandomCellDisplacement(float positionX, float positionY, float cellDistance)
 	{
-		return new Vector3 (positionX + ((cellDistance-cellSize)/2f) * Random.Range(-1f, 1f),
+		return new Vector3 (positionX + ((cellDistance-cellSize)/2f) * Random.Range(-0.75f, 0.75f),
 							0,
-		                    positionY + ((cellDistance-cellSize)/2f) * Random.Range(-1f, 1f));
+		                    positionY + ((cellDistance-cellSize)/2f) * Random.Range(-0.75f, 0.75f));
 	}
 
 	private int ConvertDoorsToDictionaryKey(bool[] doors)
@@ -206,18 +305,19 @@ public class CreateLevel : MonoBehaviour {
 		return cell;
 	}
 
-	private GameObject[] InstantiateSpanningBridge(GameObject bridgeMiddlePrefab, GameObject bridgeEndPrefab, GameObject island1, GameObject island2)
+	private GameObject[] InstantiateSpanningBridge(GameObject bridgeMiddlePrefab, GameObject bridgeEndPrefab, GameObject island1, GameObject island2, GameObject island1Anchor, GameObject island2Anchor)
 	{
 		GameObject[] bridge = new GameObject[3];
 
-		Vector3 position1 = island1.transform.position;
-		Vector3 position2 = island2.transform.position;
+		Vector3 position1 = island1Anchor.transform.position;
+		Vector3 position2 = island2Anchor.transform.position;
 
 		//Create end pieces
-		Vector3 bridgeStartPosition = Vector3.Lerp(position1, position2, 0.45f * cellSize / (position1 - position2).magnitude);
-		Vector3 bridgeEndPosition = Vector3.Lerp(position2, position1, 0.45f * cellSize / (position2 - position1).magnitude);
+		//Vector3 bridgeStartPosition = Vector3.Lerp(position1, position2, 0.45f * cellSize / (position1 - position2).magnitude);
+		//Vector3 bridgeEndPosition = Vector3.Lerp(position2, position1, 0.45f * cellSize / (position2 - position1).magnitude);
 
 		//Find height to set bridge gates at
+		/*
 		RaycastHit 	hitIsland1;
 		RaycastHit	hitIsland2;
 		Vector3		positionIsland1 = Vector3.zero;
@@ -226,16 +326,16 @@ public class CreateLevel : MonoBehaviour {
 		if (Physics.Raycast(bridgeStartPosition + Vector3.up * 20f, Vector3.down, out hitIsland1, 100.0F))
 			positionIsland1 = hitIsland1.point;
 		if (Physics.Raycast(bridgeEndPosition 	+ Vector3.up * 20f, Vector3.down, out hitIsland2, 100.0F))
-			positionIsland2 = hitIsland2.point;
+			positionIsland2 = hitIsland2.point;*/
 
-		Vector3 midPoint = (positionIsland1 + positionIsland2) * 0.5f;
+		Vector3 midPoint = (position1 + position2) * 0.5f;
 		
 		//Calculate scale/rotation
-		float spanningScale = Vector3.Distance(positionIsland1, positionIsland2);
-		Quaternion rotation = Quaternion.LookRotation(positionIsland2 - positionIsland1, Vector3.up);
+		float spanningScale = Vector3.Distance(position1, position2);
+		Quaternion rotation = Quaternion.LookRotation(position1 - position2, Vector3.up);
 
-		bridge[0] = (GameObject) Instantiate(bridgeEndPrefab, positionIsland1, rotation); 
-		bridge[1] = (GameObject) Instantiate(bridgeEndPrefab, positionIsland2, rotation); 
+		bridge[0] = (GameObject) Instantiate(bridgeEndPrefab, position1, rotation); 
+		bridge[1] = (GameObject) Instantiate(bridgeEndPrefab, position2, rotation); 
 
 		//Add the bridge gates to the activator
 		SpawnActivator island1Activator = island1.GetComponentInChildren<SpawnActivator>();
