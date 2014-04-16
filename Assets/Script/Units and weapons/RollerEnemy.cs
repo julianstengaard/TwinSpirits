29f6146c8fb7dfd7a9c35c7755c405146297c6d5
Mathias Vielwerth <onimishra@gmail.com>
2014-04-16 16:12:56 +0200

    - Fix: Melee Pumpkin's attack is now dangerous and inert at the right time - Change: Rollers can now roll through everything - Add: A new layer, that the rollers are on when rolling, that only collides with player and it's weapon.

--- Assets/Script/Units and weapons/RollerEnemy.cs
+++ Assets/Script/Units and weapons/RollerEnemy.cs
@@ -3,9 +3,21 @@ using System.Collections;
 using RAIN.Core;
 
 public class RollerEnemy : BaseEnemy {
+	private LayerMask lastLayer;
 
 	protected new void Start() {
 		base.Start ();
 		AddEffectToWeapons(new Knockback(60));
+	}
+	
+	public new void MakeDangerous() {
+		base.MakeDangerous();
+		lastLayer = gameObject.layer;
+		gameObject.layer = LayerMask.NameToLayer("RollingUnits");
+	}
+
+	public new void MakeInert() {
+		base.MakeInert();
+		gameObject.layer = lastLayer;
 	}
 }
