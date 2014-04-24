af709b18e244f65e767a7006c705c69a43f26528
julianstengaard <julianstengaard@gmail.com>
2014-04-24 18:44:15 +0200

    - Add: New sounds.  - Add: Difficulty modifier in menu.

--- Assets/Script/Static/LevelCreationInfo.cs
+++ Assets/Script/Static/LevelCreationInfo.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 
 public class LevelCreationInfo : MonoBehaviour {
-
 	public int levelLength = 2;
 	public float spiritRegen = 2;
+	public float DamageRecievedModifier = 1f;
 }
