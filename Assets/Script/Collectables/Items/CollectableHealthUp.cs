ba9e57105fae53131452303fba6b0b0d6ba818d3
TimSkafte <tim.skafte@hotmail.com>
2014-04-24 13:40:43 +0200

    - Add: OnHit particle (pumpkin), EnemySpawn particle (generic pumpkin) - Implemented: HealthUp, buggy particle Hefaistos(pingpong), SpiritPowerShrine's faked god-rays + some other stuff.

---
+++ Assets/Script/Collectables/Items/CollectableHealthUp.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+
+public class CollectableHealthUp : Collectable {
+	public override void Collected (Hero collector)	{
+		Debug.Log("Collected fat fcuk!");
+		collector.FullHealth += 1f;
+        collector.Heal(1f);
+	}
+}
