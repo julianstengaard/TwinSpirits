9708063f61e16a1d1dcbd5ab01b0c5d58e8ed272
Julian Stengaard <julianstengaard@gmail.com>
2014-04-29 23:01:27 +0200

    - Add: Implemented new UI

---
+++ Assets/Script/UI/SyncCostLineScaler.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SyncCostLineScaler : MonoBehaviour {
+    public GameObject Left;
+    public GameObject Middle;
+    public GameObject Right;
+
+    public Vector3 LeftEnd;
+    public Vector3 RightEnd;
+
+    private bool isVisible = true;
+
+     void Update() {
+        Scale();
+     }
+
+    void Scale() {
+        Left.transform.localPosition = LeftEnd + Vector3.right * 0.2f;
+        Right.transform.localPosition = RightEnd + Vector3.left * 0.2f;
+        Middle.transform.localPosition = (LeftEnd + RightEnd) * 0.5f;
+
+        //Calculate scale/rotation
+        float spanningScale = Vector3.Distance(LeftEnd, RightEnd) - 1f;
+        Quaternion rotation = Quaternion.identity;
+
+        //Scale and rotate it
+        var s = Middle.transform.localScale;
+        s.x = spanningScale;
+        Middle.transform.localScale = s;
+        Middle.transform.localRotation = rotation;
+    }
+
+    public void SetVisible(bool b) {
+        if (b && !isVisible) {
+            isVisible = true;
+            Left.renderer.enabled = true;
+            Middle.renderer.enabled = true;
+            Right.renderer.enabled = true;
+        } else if (!b && isVisible) {
+            isVisible = false;
+            Left.renderer.enabled = false;
+            Middle.renderer.enabled = false;
+            Right.renderer.enabled = false;
+        }
+    }
+}
