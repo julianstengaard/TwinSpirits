fa8aeb5005644a862684c760d1a9afaa3b95a358
julianstengaard <julianstengaard@gmail.com>
2014-04-29 16:04:36 +0200

    - Add: Line to indicate current sync cost  - Add: Divider lines to indicate standard spirit power cost.  - Change: Bungie now only costs spirit power on activation.

--- Assets/Script/UI/SpiritMeterUI.cs
+++ Assets/Script/UI/SpiritMeterUI.cs
@@ -15,6 +15,12 @@ public class SpiritMeterUI : MonoBehaviour {
 	public Material SpiritFireIcon;
 	public Material SpiritPingPongIcon;
 
+	public GameObject SpiritMeterDivider;
+	public Vector3 P1SpiritMeterDividerMin;
+	public Vector3 P1SpiritMeterDividerMax;
+	public Vector3 P2SpiritMeterDividerMin;
+	public Vector3 P2SpiritMeterDividerMax;
+
     public Material SyncOnIcon;
     public Material SyncOffIcon;
 
@@ -25,7 +31,9 @@ public class SpiritMeterUI : MonoBehaviour {
 
     private GameObject syncIcon;
 	private GameObject p1Meter;
+	private GameObject p1SyncCostLine;
 	private GameObject p2Meter;
+	private GameObject p2SyncCostLine;
 
     private Vector3 p1MeterZero = new Vector3(-0.39f, -0f, 0.05f);
     private Vector3 p2MeterZero = new Vector3(0.39f, -0f, 0.05f);
@@ -36,6 +44,9 @@ public class SpiritMeterUI : MonoBehaviour {
 	private bool animatingSpiritPowerP1 = false;
 	private bool animatingSpiritPowerP2 = false;
 
+	private GameObject[] _p1Dividers;
+	private GameObject[] _p2Dividers;
+
 	// Use this for initialization
 	void Start () 
 	{
@@ -45,6 +56,9 @@ public class SpiritMeterUI : MonoBehaviour {
 
 		p1Meter = GameObject.Find(this.gameObject.name+"/LeftSpiritMeter/LeftSpiritMeterAmount");
 		p2Meter = GameObject.Find(this.gameObject.name+"/RightSpiritMeter/RightSpiritMeterAmount");
+
+		p1SyncCostLine = GameObject.Find(this.gameObject.name+"/LeftSpiritMeter/LeftSpiritSyncCostLine");
+		p2SyncCostLine = GameObject.Find(this.gameObject.name+"/RightSpiritMeter/RightSpiritSyncCostLine");
 	}
 
 	// Update is called once per frame
@@ -76,23 +90,80 @@ public class SpiritMeterUI : MonoBehaviour {
                 print("dafuq");
 		}
 		return false;
-
 	}
+
+	void SetSpiritMeterDividers(int playerNumber) {
+		if (playerNumber == 1) {
+			if (_p1Dividers != null) {
+				foreach(GameObject go in _p1Dividers) {
+					Destroy(go);
+				}
+			}
+			int p1divisions = (int) (100/Player1.currentSpiritPower.GetCostActivate());
+			_p1Dividers = new GameObject[p1divisions-1];
+			Vector3 distancePerDivider = (P1SpiritMeterDividerMax - P1SpiritMeterDividerMin)/p1divisions;
+			for (int i = 1; i < p1divisions; i++) {
+				_p1Dividers[i-1] = (GameObject) GameObject.Instantiate(SpiritMeterDivider, Vector3.zero, Quaternion.identity);
+				_p1Dividers[i-1].transform.parent = p1Meter.transform.parent;
+				_p1Dividers[i-1].transform.localPosition = P1SpiritMeterDividerMin + distancePerDivider * i;
+			}
+		}
+		else if (playerNumber == 2) {
+			if (_p2Dividers != null) {
+				foreach(GameObject go in _p2Dividers) {
+					Destroy(go);
+				}
+			}
+			int p2divisions = (int) (100/Player2.currentSpiritPower.GetCostActivate());
+			_p2Dividers = new GameObject[p2divisions-1];
+			Vector3 distancePerDivider = (P2SpiritMeterDividerMax - P2SpiritMeterDividerMin)/p2divisions;
+			for (int i = 1; i < p2divisions; i++) {
+				_p2Dividers[i-1] = (GameObject) GameObject.Instantiate(SpiritMeterDivider, P2SpiritMeterDividerMin + distancePerDivider * i, Quaternion.identity);
+				_p2Dividers[i-1].transform.parent = p2Meter.transform.parent;
+				_p2Dividers[i-1].transform.localPosition = P2SpiritMeterDividerMin + distancePerDivider * i;
+			}
+		}
+	}
+
 	void UpdateSpiritMeter(int playerNumber) {
 		if (playerNumber == 1) {
 			float spiritAmount = Player1.currentSpiritAmount/100f;
 			p1Meter.transform.localScale 	= new Vector3(spiritAmount*0.85f, 0.5f, 1);
             p1Meter.transform.localPosition = Vector3.Lerp(p1MeterZero, p1Zero, spiritAmount);
 			ColorizeSpiritMeter(p1Meter, Player1.currentSpiritPower, spiritAmount*100f);
+
+			if (Player1.currentSpiritPower.GetCostActivateSync() <= Player1.currentSpiritAmount)
+				p1SyncCostLine.renderer.enabled = true;
+			else
+				p1SyncCostLine.renderer.enabled = false;
+
+			//Set the cost
+			float cost = Player1.currentSpiritPower.GetCostActivateSync()/100f;
+			p1SyncCostLine.transform.localScale	= new Vector3(cost*0.85f, 0.15f, 1);
+			Vector3 targetPosition = p1SyncCostLine.transform.position;
+			targetPosition.x = p1Meter.transform.position.x + p1Meter.transform.localScale.x/2f - p1SyncCostLine.transform.localScale.x/2f;
+			p1SyncCostLine.transform.position = targetPosition;
 		}
 		else if (playerNumber == 2) {
 			float spiritAmount = Player2.currentSpiritAmount/100f;
 			p2Meter.transform.localScale = new Vector3(spiritAmount*0.85f, 0.5f, 1);
             p2Meter.transform.localPosition = Vector3.Lerp(p2MeterZero, p2Zero, spiritAmount); 
 			ColorizeSpiritMeter(p2Meter, Player2.currentSpiritPower, spiritAmount*100f);
+
+			if (Player2.currentSpiritPower.GetCostActivateSync() <= Player2.currentSpiritAmount)
+				p2SyncCostLine.renderer.enabled = true;
+			else
+				p2SyncCostLine.renderer.enabled = false;
+
+			//Set the cost
+			float cost = Player2.currentSpiritPower.GetCostActivateSync()/100f;
+			p2SyncCostLine.transform.localScale	= new Vector3(cost*0.85f, 0.15f, 1);
+			Vector3 targetPosition = p2SyncCostLine.transform.position;
+			targetPosition.x = p2Meter.transform.position.x - p2Meter.transform.localScale.x/2f + p2SyncCostLine.transform.localScale.x/2f;
+			p2SyncCostLine.transform.position = targetPosition;
 		}
 	}
-
+	
 	void ColorizeSpiritMeter(GameObject spiritMeter, SpiritPower spiritPower, float amount) {
 		if (amount < spiritPower.GetCostActivate())
 			spiritMeter.renderer.material.SetColor("_Color", Color.red);
@@ -104,7 +175,7 @@ public class SpiritMeterUI : MonoBehaviour {
 
     public void UpdateSyncIcon()
     {
-        if (Player1.currentSpiritAmount >= 100f && Player2.currentSpiritAmount >= 100f)
+		if (Player1.currentSpiritAmount >= Player1.currentSpiritPower.GetCostActivateSync() && Player2.currentSpiritAmount >= Player2.currentSpiritPower.GetCostActivateSync())
         {
             syncIcon.renderer.material = SyncOnIcon;
         }
@@ -177,6 +248,9 @@ public class SpiritMeterUI : MonoBehaviour {
 
 	private IEnumerator AnimateIconTransition(GameObject oldIcon, GameObject newIcon, int playerNumber) {
 		float time = 0.8f;
+
+		//Update the dividers
+		SetSpiritMeterDividers(playerNumber);
 
 		if (playerNumber == 1)
 			animatingSpiritPowerP1 = true;
