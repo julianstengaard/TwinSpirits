58bbe5f691a13096c1c38a7e32288f9224497a1f
TimSkafte <tim.skafte@hotmail.com>
2014-04-30 16:42:44 +0200

    -fix: improved menu scene -add: Lightning graphics for ultimate and explosion on normal ball throw. -add: Hera damage graphics for ultimate -fix: Health up item (now 1 heart, up from ½) and tweened text on pickup.

--- Assets/Script/Collectables/Items/CollectableHealthUp.cs
+++ Assets/Script/Collectables/Items/CollectableHealthUp.cs
@@ -4,8 +4,10 @@ using System.Linq;
 
 public class CollectableHealthUp : Collectable {
 	public override void Collected (Hero collector)	{
-		Debug.Log("Collected fat fcuk!");
-		collector.FullHealth += 1f;
-        collector.Heal(1f);
+
+		collector.FullHealth += 2f;
+        collector.Heal(2f);
+		base.CreatePopUpText("Health up", collector);
+		GameObject.Destroy(gameObject);
 	}
 }
