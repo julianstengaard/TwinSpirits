2aca6416f50b902b32a2a0befda9f19e573f1af3
Mathias Vielwerth <onimishra@gmail.com>
2014-05-02 16:22:25 +0200

    - Add: Attack planes!! - Change: Trail on swords are only when animating - Change: Higher colliders on the bridge openings - Update: Home island with new players - Update: Home island removed sounds from shrines

---
+++ Assets/Script/Units and weapons/WeaponPlane.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class WeaponPlane : Weapon {
+	private float _startTime;
+	private float _speed = 20;
+
+	private Vector3 _startPoint;
+	private Vector3 _endPoint;
+
+	public new void Start() {
+		base.Start();
+		_startTime = Time.time;
+		_startPoint = transform.position;
+		_endPoint = transform.position + Body.transform.forward;
+	}
+
+	public void Update() {
+		var forward = transform.up * -0.25f;
+		var deltaTime = (Time.time - _startTime) / (1/_speed);
+		transform.position = Vector3.Lerp(_startPoint, _endPoint, deltaTime);
+
+		if(deltaTime > 1)
+			GameObject.Destroy(gameObject);
+	}
+}
