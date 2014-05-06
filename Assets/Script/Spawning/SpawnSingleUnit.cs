1ffb93926305e642cc1d615bb31f4c2641362151
Mathias Vielwerth <onimishra@gmail.com>
2014-05-06 11:56:09 +0200

    - Add: Particle spawn effect before enemies appear - Cleanup...

--- Assets/Script/Spawning/SpawnSingleUnit.cs
+++ Assets/Script/Spawning/SpawnSingleUnit.cs
@@ -1,17 +1,36 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 public class SpawnSingleUnit : Spawner {
 	public Transform FacePoint;
 	public BaseUnit[] SpawnableUnits;
 
+	private GameObject particles;
+
+	public void Start() {
+		var particlesType = Resources.Load<GameObject>("particle_enemy_spawn");
+
+		particles = (GameObject) GameObject.Instantiate(particlesType, transform.position, Quaternion.identity);
+		if(particles == null)
+			print ("Blow it out your ass...");
+	}
+
 	public override void Spawn() {
-	    if (_enemiesToSpawn > 0) {
-	        var unitType = SpawnableUnits[Random.Range(0, SpawnableUnits.Length - 1)];
-	        var unit = (BaseUnit) GameObject.Instantiate(unitType, transform.position, Quaternion.identity);
-            //print("spawned a: " + unit.name);
-	        if (FacePoint != null)
-	            unit.transform.LookAt(FacePoint);
+		if (_enemiesToSpawn > 0) {
+			foreach(var particle in particles.GetComponentsInChildren<ParticleSystem>())
+				particle.Play();
+			StartCoroutine(DelaySpawn());
 	    }
+	}
+
+	public IEnumerator DelaySpawn() {
+		yield return new WaitForSeconds(2);
+
+		var unitType = SpawnableUnits[Random.Range(0, SpawnableUnits.Length - 1)];
+		var unit = (BaseUnit) GameObject.Instantiate(unitType, transform.position, Quaternion.identity);
+		//print("spawned a: " + unit.name);
+		if (FacePoint != null)
+			unit.transform.LookAt(FacePoint);
 	}
 
     public override bool SetEnemiesToSpawn(int enemies) {
