3b2a99ec4dd8ac76dbbc4899df09aab7abb133f6
TimSkafte <tim.skafte@hotmail.com>
2014-05-08 21:36:49 +0200

    -add: material offset scrolling (spirit meter) & rough particle setup -add: particle attractor setup for revive particle system

---
+++ Assets/Particles/test scripts/ParticleAttractor.cs
@@ -0,0 +1,43 @@
+﻿// ParticleAttractor.cs
+
+using UnityEngine;
+
+using System.Collections;
+
+
+
+public class ParticleAttractor : MonoBehaviour {
+	public ParticleSystem[] systems;
+	private float radius = 10f;
+	public float power = 1f;
+	public float maxPower = 100f;
+	public float speed = 5f;
+
+	void Start(){
+	}
+		
+	void Update(){
+		for(int j = 0; j<systems.Length; j++){
+			ParticleSystem system = systems[j];
+			Transform t = system.transform;
+			ParticleSystem.Particle[] particles = new ParticleSystem.Particle[system.particleCount];
+			system.GetParticles(particles);
+			for(int i=0; i<system.particleCount; i++){
+				Vector3 pos = particles[i].position;//t.TransformPoint();
+				Debug.DrawLine(pos, transform.position, new Color(1,0.5f, 0.5f, 0.25f));
+				Vector3 dir = transform.position - pos;
+				float amount = (1 - dir.magnitude / radius) * power / maxPower;
+				dir.Normalize();
+
+				/*
+                float amount = Mathf.Clamp01(dir.magnitude / radius);
+                dir = dir.normalized * power;
+                particles[i].velocity = Vector3.Lerp(particles[i].velocity, dir, amount * Time.deltaTime);
+                */
+
+				particles[i].velocity = Vector3.Lerp(particles[i].velocity, dir * speed, amount * Time.deltaTime);
+			}
+			system.SetParticles(particles, particles.Length);
+		}
+	}
+}
