a6962667a8cc412de6dd5b83b4c71a4b0e526fb3
TimSkafte <tim.skafte@hotmail.com>
2014-05-11 20:10:03 +0200

    -add: player color gems in UI -change: roughly reworked gate_island & tree_circle islands -add: extracted prefab combinations from noplacelikehome for further lvl design -change: fixed damage items (DamageUp = 5  GlassCannon = 10)

--- Assets/Script/Collectables/Items/CollectableDamageUp.cs
+++ Assets/Script/Collectables/Items/CollectableDamageUp.cs
@@ -4,7 +4,7 @@ public class CollectableDamageUp : Collectable {
 	public override void Collected (Hero collector)	{
 		var weapons = collector.GetComponentsInChildren<Weapon>();
 		foreach(var w in weapons)
-			w.AddEffect(new Damage(1));
+			w.AddEffect(new Damage(5));
         base.CreatePopUpText("Damage up", collector);
 		//GameObject.Destroy(gameObject);
 	}
--- Assets/Script/Collectables/Items/CollectableGlassCannon.cs
+++ Assets/Script/Collectables/Items/CollectableGlassCannon.cs
@@ -2,7 +2,7 @@
 
 public class CollectableGlassCannon : Collectable {
 	public override void Collected (Hero collector)	{
-		collector.AddEffectToWeapons(new Damage(2));
+		collector.AddEffectToWeapons(new Damage(10));
         collector.FullHealth = Mathf.Max(2, collector.FullHealth - 2);
         base.CreatePopUpText("Glass cannon", collector);
 		//GameObject.Destroy(gameObject);
