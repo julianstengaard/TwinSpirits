31ccf296333596a78e9afd58bdbe934fc9d01258
julianstengaard <julianstengaard@gmail.com>
2014-05-12 15:06:11 +0200

    - Added code for letting ice sword change color of enemies, but their shader won't really colorize them properly

--- Assets/Script/Effects/SlowdownMovementSpeed.cs
+++ Assets/Script/Effects/SlowdownMovementSpeed.cs
@@ -14,17 +14,27 @@ public class SlowdownMovementSpeed : Effect
 
 	public override void DoEffect (BaseUnit target, GameObject source, Vector3 attackPosition, ref float damage)
 	{
-		Debug.Log("Changing movement speed from: " + target.movementSpeedBuff + " to " +  (target.movementSpeedBuff + slowAmount));
+		//Debug.Log("Changing movement speed from: " + target.movementSpeedBuff + " to " +  (target.movementSpeedBuff + slowAmount));
 	}
 
 	public override IEnumerator DoEffectCoroutine (BaseUnit target, GameObject source, Vector3 attackPosition)
 	{
 		if(Random.Range(0f, 1f) < hitChance) {
 			target.SetMovementSpeedBuff(-slowAmount);
+			/* 
+			float startTime = Time.time;
+			Color slowColor = new Color(0f, 0f, 1f);
+			while (startTime + duration > Time.time) {
+				float pct = Mathf.PingPong((Time.time - startTime) * 10f, 1f);
+				target.SetDamageBodyColor(slowColor, pct);
+				yield return new WaitForEndOfFrame();
+			}
+			target.SetDamageBodyColor(slowColor, 0f);
+			*/
 			yield return new WaitForSeconds(duration);
+
 			target.SetMovementSpeedBuff(slowAmount);
 		}
-		
 		yield return null;
 	}
 
